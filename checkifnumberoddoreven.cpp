58fd74f677867566285ff46aec5dc3b180263a3c
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-16 14:50:14 +0200

    Create checkifnumberoddoreven.cpp

---
+++ checkifnumberoddoreven.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int x;
+	cin>>x;
+	if (x % 2 == 0 ){
+		cout<<"the number is even"<<endl;
+	}else
+		cout<<"the number is odd"<<endl;
+
+	return 0;
+}
