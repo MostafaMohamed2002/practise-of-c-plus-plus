f27e7fee1837a5ae79daa1c3c7da41e816fce4bd
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-16 13:09:56 +0200

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	cout<<"sizeof(char)"<<sizeof(char)<<endl;
+	cout<<"sizeof(int)"<<sizeof(int)<<endl;
+	cout<<"sizeof(float)"<<sizeof(float)<<endl;
+	cout<<"sizeof(long long)"<<sizeof(long long)<<endl;
+	cout<<"sizeof(double)"<<sizeof(double)<<endl;
+	return 0;
+}
