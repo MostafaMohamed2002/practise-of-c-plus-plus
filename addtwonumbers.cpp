2bf6f52ee723f8a0dfc623a3a0b3b3ac483cda0b
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-16 13:09:49 +0200

    Create addtwonumbers.cpp

---
+++ addtwonumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	double x;
+	double y;
+	double sum;
+	cout<<"enter value of x and y "<<endl;
+	cin>>x>>y;
+	sum = x + y ;
+	cout<<"This sum of x + y ="<<sum<<endl;
+	return 0;
+
+}
