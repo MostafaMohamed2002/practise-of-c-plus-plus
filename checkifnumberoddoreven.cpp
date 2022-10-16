b2e679e02c2abd2793d22a9064dd223eb5680378
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-17 01:38:25 +0200

    New File IF char  vowel or constant

--- checkifnumberoddoreven.cpp
+++ checkifnumberoddoreven.cpp
@@ -4,10 +4,9 @@ int main(int argc, char const *argv[])
 {
 	int x;
 	cin>>x;
-	if (x % 2 == 0 ){
+	if (x % 2 == 0 )
 		cout<<"the number is even"<<endl;
-	}else
-		cout<<"the number is odd"<<endl;
-
+  else
+  	cout<<"the number is odd"<<endl;
 	return 0;
 }
